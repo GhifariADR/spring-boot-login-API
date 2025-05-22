00221b224a1d6295b3649363e5336d576be665ab
Muhammad Ghifari Adrian <ghifariadr6@gmail.com>
2025-05-22 12:33:31 +0700

    update CORS

---
+++ src/main/java/com/example/auth/Config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.auth.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // semua path
+						.allowedOrigins("http://localhost:5173") // frontend kamu
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+}
