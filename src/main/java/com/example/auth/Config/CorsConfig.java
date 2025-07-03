73c87c5e8b79b379ab39c59d0e1e903344b5a3b2
Muhammad Ghifari Adrian <ghifariadr6@gmail.com>
2025-07-03 13:53:27 +0700

    update CORS config

--- src/main/java/com/example/auth/Config/CorsConfig.java
+++ src/main/java/com/example/auth/Config/CorsConfig.java
@@ -6,19 +6,28 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**") // semua path
-						.allowedOrigins("http://localhost:5173", "https://auth-fe-sandy.vercel.app/", "https://auth-fe.up.railway.app/") // frontend kamu
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOriginPatterns("https://auth-fe.up.railway.app")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**") // semua path
+//						.allowedOrigins("http://localhost:5173", "https://auth-fe-sandy.vercel.app/", "https://auth-fe.up.railway.app/") // frontend kamu
+//						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//						.allowedHeaders("*")
+//						.allowCredentials(true);
+//			}
+//		};
+//	}
 }
